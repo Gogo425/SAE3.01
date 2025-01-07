68828313fb9859f4c4bf3f222b0d7cae2c970bb8
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2025-01-07 11:04:59 +0100

    :art: create home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Accueil</title>
+    </head>
+    <body>
+        <p><a href=""><img src="../image/logo.png" alt="logo"></p>
+        <button>Profil</button>
+        <button>Déconnexion</button>
+        <button>Liste d'élèves et d'initiateurs</button>
+        <button>Liste de formations</button>
+        <button>Consulter ma formation</button>
+        <button>Consulter l'emploi du temps</button>
+    </body>
+</html>
