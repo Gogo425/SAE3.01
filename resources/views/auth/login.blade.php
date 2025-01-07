78e5ffbe8039449b84f23f20a1045716183407c6
Lucas Demaimay <demaima231@campus.unicaen.fr>
2025-01-07 15:40:31 +0100

    :lipstick: Style of the form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="../js/tailwind.config.js"></script>
     <title>Connexion</title>
 </head>
 <body>
@@ -11,18 +13,19 @@
         @csrf
 
         <label for="EMAIL">Email</label>
-        <input type="EMAIL" id="EMAIL" name="EMAIL" required value={{old('EMAIL')}} >
+        <input type="EMAIL" id="EMAIL" name="EMAIL" class="border-solid border-2" required value={{old('EMAIL')}} >
         @error('EMAIL')
             {{$message}}
         @enderror
 
         <label for="password">Mot de passe</label>
-        <input type="password" id="password" name="password" required>
+        <input type="password" id="password" name="password" class="border-solid border-2" required>
         @error('password')
             {{$message}}
         @enderror
 
-        <input type="submit" value="Se Connecter">
+        <input type="submit" value="Se Connecter" >
+
     </form>
 </body>
 </html>
