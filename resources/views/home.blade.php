b34813d2fc210bb0e85367bd0a2a038ce132ade4
BEUVE <beuve225@unicaen.fr>
2025-01-08 23:56:20 +0100

    :lipstick: add to background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,11 @@
 @extends('base')
 
-
-
 @section('title', 'acceuil')
 
 @section('content')
-
         <button>Liste d'élèves et d'initiateurs</button>
         <button>Liste de formations</button>
         <button>Consulter ma formation</button>
         <button>Consulter l'emploi du temps</button>
 
-
-
 @endsection
-
-
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,29 @@
-@extends('base')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title> 
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="../js/tailwind.config.js"></script>
+</head>
 
+<body style="background-image: url('{{ asset('img/fondEcran.jpg') }}');" class="bg-cover bg-center place-self-center  ">
+@include("header")
+<p>{{Auth::check()}}</p>
+<form method="post" action="/login">
+    @csrf
+    <label for="email"> Email </label>
+    <input type="email" name="email" id="email">
+    <label for="password"> Password </label>
+    <input type="password" name="password" id="password">
 
+    <input type="submit" value="Se Connecter">
+    @include("footer")
+</form>
 
-@section('title', 'acceuil')
+</body>
 
-@section("content")
+    
 
-
-    <p>{{Auth::check()}}</p>
-    <form method="post" action="/login">
-        @csrf
-
-        <label for="email"> Email </label>
-        <input type="email" name="email" id="email">
-        <label for="password"> Password </label>
-        <input type="password" name="password" id="password">
-
-        <input type="submit" value="Se Connecter">
-    </form>
-@endsection
 </html>
