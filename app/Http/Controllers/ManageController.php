7d1544e6168e15955b7b6de170d23ecd5b86ad0b
soren.GAUDRE <soren>
2025-01-08 17:58:20 +0100

    amelioration

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -6,17 +6,22 @@ use Illuminate\Http\Request;
 use App\Models\Students;
 use App\Models\Initiators;
 use App\Models\Persons;
+use App\Http\Controllers\DB;
 
 class ManageController extends Controller
 {
     public function index()
     {
         
-        $students = Students::all();
-        $initiators = Initiators::all();
-        $persons = Persons::all();
+        $students = DB::table('students')->get();
+        $initiators = DB::table('initiators')->get();
+        $persons = DB::table('persons')->get();
+
+
         
     
-        return view('manage', compact('students'));
+    return view('manage', ['students' => $students,  'initiators' => $initiators, 'persons' => $persons]);
+
     }
+    
 }
