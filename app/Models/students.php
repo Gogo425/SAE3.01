844816f8a045edadffcba6415e74ec3a7aeee555
Felix Pinson <felixpinson@etu.unicaen.fr>
2025-01-08 18:40:05 +0100

    :bug: fix merge bug

--- app/Models/students.php
+++ app/Models/students.php
@@ -5,17 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Students extends Model
+class Student extends Model
 {
     use HasFactory;
 
     protected $table = 'students';
-    protected $fillable = ['id_per', 'id_level', 'id_formation'];
+    protected $filliable = ['id_per', 'id_level', 'id_formation'];
 
     public function user(){
         return $this->belongsTo(Persons::class);
     }
-
-    public $timestamps = false;
 
 }
