3b3fa7ee298162ff3dbe541f4fe1da3bd6596f88
BEUVE <beuve225@unicaen.fr>
2025-01-08 10:38:54 +0100

    :package: header added to home page

---
+++ resources/views/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <menu>
+        <p><a href=""><img src="./img/logo.png" alt="logo"></p>
+        <button>Profil</button>
+        <button>Déconnexion</button>
+    </menu>
+</header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>Accueil</title>
-    </head>
-    <body>
-        <p><a href=""><img src="../image/logo.png" alt="logo"></p>
-        <button>Profil</button>
-        <button>Déconnexion</button>
+@extends("header")
+@section('title', 'acceuil')
+
+@section('content')
+
+      
         <button>Liste d'élèves et d'initiateurs</button>
         <button>Liste de formations</button>
         <button>Consulter ma formation</button>
         <button>Consulter l'emploi du temps</button>
-    </body>
-</html>
+@endsection
