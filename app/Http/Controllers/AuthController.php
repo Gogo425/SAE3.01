2f236ae0af2d922b9b7abbb22c7fe047eae99dd1
Lucas Demaimay <demaima231@campus.unicaen.fr>
2025-01-08 10:19:04 +0100

    :ambulance: Help

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    
+    public function login(){
+        return view('login');
+    }
+
+    public function doLogin(){
+        dd("bien");
+
+        return view('login');
+    }
+
+
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label for="email"> Email </label>
+        <input type="email" name="email" id="email">
+        <label for="password"> Password </label>
+        <input type="password" name="password" id="password">
+        <input type="submit" value="Se Connecter">
+    </form>
+</body>
+</html>
