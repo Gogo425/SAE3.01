a4b0b89302e0acdee46c9967cfbc4bbf956bd458
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2025-01-09 13:00:27 +0100

    :truck: home link

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <menu>
         <div class="flex items-center justify-between p-4 bg-gray-100">
-            <img src="./img/logo.png" alt="logo" class="w-16 h-16">
+            <a href="/" title="accueil"> <img src="./img/logo.png" alt="logo" class="w-16 h-16"> </a>
             <div class="flex space-x-4">
                 <a href="/profile"><button class="pink">Profil</button></a>
                 <a href="/logout"><button class="dark_blue">Déconnexion</button></a>
