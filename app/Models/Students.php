27deaed8f32cc4395aae9f1b5ffcafb63b605840
Felix Pinson <felixpinson@etu.unicaen.fr>
2025-01-09 09:03:33 +0100

    :construction: Correction bug 2.0

---
+++ app/Models/Students.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Students extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'students';
+    protected $filliable = ['id_per', 'id_level', 'id_formation'];
+
+    public function user(){
+        return $this->belongsTo(Persons::class);
+    }
+}
