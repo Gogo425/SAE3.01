6ae7c5bc41e8fcd1fba56c4ca6629f0b077448c2
Lucas Demaimay <demaima231@unicaen.fr>
2025-01-09 19:38:34 +0100

    :poop: add Sessions file

---
+++ app/Models/Sessions.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Sessions extends Model
+{
+    public $table = 'sessions';
+    public $primary_key = 'id_abilities';
+    public $incrementing = true;
+    public $timestamps = false;
+    function selectAllTable(){
+        $abilities = sessions::all()->sortBy('DATE_SESSION');
+        return $abilities;
+    }
+    function selectByForamtion($formation){
+        $abilities = sessions::where('ID_FORMATION',$formation)->get();
+        return $abilities;
+    }
+    function getByDate($date){
+        $session = sessions::where('DATE_SESSION',$date)->select('ID_SESSIONS')->get();
+        return $session;
+    }
+}
