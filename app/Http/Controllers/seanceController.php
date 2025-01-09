98a08f4f131706c2dd76b4416ae5e06bd1876535
Felix Pinson <felixpinson@etu.unicaen.fr>
2025-01-09 16:23:10 +0100

    :hammer: Add condition for insertion data

--- app/Http/Controllers/seanceController.php
+++ app/Http/Controllers/seanceController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Sessions;
 use App\Models\Works;
+use App\Models\Evaluations;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\DateTime;
+use Illuminate\Support\Arr;
 
 class SeanceController extends Controller
 {
@@ -22,10 +27,29 @@ class SeanceController extends Controller
                                         ->join('formations', 'formations.id_formation', '=', 'students.id_formation')
                                         ->where("students.id_formation", $form)->get();
 
-        $abilities = DB::table('formations')->join('levels', 'formations.id_level', '=', 'levels.id_level')
-                                            ->join('skills', 'skills.id_level', '=', 'levels.id_level')
-                                            ->join('abilities', 'skills.id_skills', '=', 'abilities.id_skills')
-                                            ->where("formations.id_formation", $form)->get();
+                                        $abilities = array();
+
+                                        $tabAbilities = DB::table('formations')->join('levels', 'formations.id_level', '=', 'levels.id_level')
+                                                                                ->join('skills', 'skills.id_level', '=', 'levels.id_level')
+                                                                                ->join('abilities', 'skills.id_skills', '=', 'abilities.id_skills')
+                                                                                ->where("formations.id_formation", $form)->distinct('ID_ABILITIES')->get();
+                                
+                                
+                                        foreach($students as $student) {
+                                
+                                            $studentAbilities = array();
+                                
+                                            $evaluation = new Evaluations();
+                                
+                                            foreach($tabAbilities as $abiliti) {
+                                                $rep = $evaluation->getEvaluationsStudent($student->ID_PER, $abiliti->ID_ABILITIES);
+                                                if(!$rep) {
+                                                    array_push($studentAbilities, $abiliti);
+                                                }
+                                            }
+                                
+                                            array_push($abilities, $studentAbilities);
+                                        }
 
         $initiators = DB::table('trains')->join('initiators', 'trains.id_per_initiator', '=', 'initiators.id_per')
                                           ->join('persons', 'persons.id_per', '=', 'initiators.id_per')
