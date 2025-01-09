2864fd3904e40f6e44c1a6201c19a88e495135da
Lucas Demaimay <demaima231@campus.unicaen.fr>
2025-01-09 13:34:00 +0100

    Fix main 2

--- app/Models/abilities.php
+++ app/Models/abilities.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Abilities extends Model
 {
     public $table = 'abilities';
@@ -12,20 +9,16 @@ class Abilities extends Model
     public $incrementing = true;
     public $timestamps = false;
 
-
     function selectAllTable(){
         $abilities = abilities::all()->sortBy('ID_SKILLS');
         return $abilities;
     }
-
     function selectBySkill($skill){
         $abilities = abilities::where('ID_SKILLS',$skill)->orderBy('ID_ABILITIES', 'asc')->get();
         return $abilities;
     }
-
     function countBySkill($skill){
         $abilities = abilities::where('ID_SKILLS',$skill)->count();
         return $abilities;
     }
-
 }
