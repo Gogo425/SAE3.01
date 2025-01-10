768751e0dc984b585eac1bd3e70d3a8c2972267d
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2025-01-10 11:09:55 +0100

    :art: cssCheck

--- resources/views/creationSeance.blade.php
+++ resources/views/creationSeance.blade.php
@@ -6,6 +6,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/sessionStyle.css') }}">
 
         <title>Création d'une séance</title>
     </head>
