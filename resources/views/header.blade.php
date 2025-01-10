9b44cada8eaa44977cc72896a8be085c2dbbcbff
Hugo JAUBERT <hjaubert7@gmail.com>
2025-01-10 10:05:08 +0100

    modif navbar

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -15,7 +15,9 @@
                         echo "
                             <a href=".route('liste')."><button class='px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600'>Liste des utilisateurs</button></a>
                             <a href=".route('formation')."><button class='px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600'>Liste des formations</button></a>
-                            <a href=".route('tableStudent')."><button class='px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600'>Bilan des élèves</button></a>";
+                            <a href=".route('tableStudent')."><button class='px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600'>Bilan des élèves</button></a>
+                            <a href=".route('abilities.index')."><button class='px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-600'>Gestion des aptitudes</button></a>
+                            ";
                     }
                     if($trainingManager->count == 1){
                         echo "
